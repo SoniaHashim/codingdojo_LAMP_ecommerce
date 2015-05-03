f114b31cc73b86fd6a19933e0ec0488e864ae0b6
Sonia Hashim <soniapriyahashim@gmail.com>
2015-05-03 11:44:12 -0700

    Trying to find folders?

---
+++ GraphicNovels_eCommerce/application/views/users_index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Users Index - Products Display</title>
+	<link rel="stylesheet" type="text/css" href="/assets/users_main.css">
+	<style type="text/css"></style>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+	<script type="text/javascript"></script>
+</head>
+<body>
+	<div class='header'>
+	<!-- END OF HEADER -->
+	</div>
+	<div class='container'>
+		<div class='products-filter'>
+		</div>
+		<div class='products-display'>
+		</div>
+	<!-- END OF CONTAINER -->
+	</div>
+</body>
+</html>
