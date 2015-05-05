19fff5a185a1d17e63ee33f59724b6ee751d732e
Sonia Hashim <soniapriyahashim@gmail.com>
2015-05-05 11:04:30 -0700

    Minor changes - we should be at the first user page with these edits'

--- GraphicNovels_eCommerce/application/config/routes.php
+++ GraphicNovels_eCommerce/application/config/routes.php
@@ -1,6 +1,6 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$route['default_controller'] = "users_index";
+$route['default_controller'] = "products";
 $route['404_override'] = '';
 
 //end of routes.php
