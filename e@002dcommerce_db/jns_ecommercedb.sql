349d6d3a12724ace07d9a39cabca6007301f128a
JV Estolas <fortis201@gmail.com>
2015-05-07 16:33:51 -0700

    replaced old sql file with latest version, admin_products_show search and delete now functional

--- e@002dcommerce_db/jns_ecommercedb.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-products.id as id, products.name as name, images.file_path as image, products.description as description, products.price as price
-FROM products_has_categories 
-LEFT JOIN products ON products.id = products_has_categories.product_id 
-LEFT JOIN categories ON categories.id = products_has_categories.category_id 
-LEFT JOIN images ON products.image_id = images.id
-WHERE name LIKE '%%' AND  category LIKE '%Marvel%' 
-ORDER BY id ASC
-LIMIT 0, 5 
